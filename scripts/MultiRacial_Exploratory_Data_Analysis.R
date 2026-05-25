b89919fcdb6812ecf329420336990d47fb2cab1b
Cathea Carey <cmc37@uw.edu>
2026-05-25 09:12:41 -0700

    initial copy of project (test)

---
+++ scripts/MultiRacial_Exploratory_Data_Analysis.R
@@ -0,0 +1,14 @@
+###### Cathea M. Carey, MPH #######
+##### Exploratory Data Analysis (EDA) on Multi-Racial and Erasure
+
+#### Stems from initial conversations and analysis of 2022 Unsheltered PIT Count
+#### data set that contains over 600 qualitative responses to a question asking 
+#### for the respondent (individual experiencing unsheltered or HUD defined
+#### homelessness in a place not meant for human habitation) self-identification
+#### for race/ethnicity (gender was also included), prior to the 2024 Data 
+#### Standards Update to include Hispanic/Latino/a/x as a racial category 
+#### (still keeping Hispanic/Latino/a/x as an ethnicity) and creating Middle
+#### Eastern or North African.
+
+#### Question: How does "more than one race" change demographics? 
+
