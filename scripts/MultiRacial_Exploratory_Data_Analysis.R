58b09b77ce029d1b023d9dde53d2c58010cad7aa
Cathea Carey <cmc37@uw.edu>
2026-05-25 17:18:43 -0700

    variables

--- scripts/MultiRacial_Exploratory_Data_Analysis.R
+++ scripts/MultiRacial_Exploratory_Data_Analysis.R
@@ -8,7 +8,37 @@
 #### for race/ethnicity (gender was also included), prior to the 2024 Data 
 #### Standards Update to include Hispanic/Latino/a/x as a racial category 
 #### (still keeping Hispanic/Latino/a/x as an ethnicity) and creating Middle
-#### Eastern or North African.
+#### Eastern or North African. NOTE: Middle Eastern or North African was not
+#### included in 2020 Census; not projected/estimated in Census tables.
 
 #### Question: How does "more than one race" change demographics? 
 
+#### Aim: Visualize census data using Office of Management and Budget (OMB)
+#### data standard and then show the data in combination of 'racialized' groups.
+
+
+#Create Variables
+#labels for race
+R_labels <- c(
+  AAA   = "B03002_006",
+  AIANI = "B03002_005",
+  BAAA  = "B03002_004",
+  HL    = "B03002_012", 
+  #MENA NOT PROJECTED OR ESTIMATED
+  NHPI  = "B03002_007",
+  W     = "B03002_003",
+  SORA  = "B03002_008", #Label for "Some Other Race Alone" 
+  TOMR  = "B03002_009"  #Label for "Two or More Races
+)
+
+wa_race <- get_acs(
+  geography = "county",
+  state = "WA",
+  variables = R_labels,
+  year = 2023
+)
+
+
+#racialidentity_combo <-
+  
+#racialidentity_mutexcl <- 
--- scripts/run_instructions.R
+++ scripts/run_instructions.R
@@ -1,7 +1,25 @@
+#Laod packages
+library(scales)
+library(gt)
+library(sf)
+library(tmap)
+library(tigris)
+library(tidygeocoder)
+library(httr)
+library(jsonlite)
+library(shiny)
+library(flexdashboard)
+
+#Census API - run lines 16-17 to initiate connection
+library(tidyverse)
+library(tidycensus)
+#my_key <- readLines("/Users/catheacarey/Documents/KCRHA Work/census_key.txt")
+#census_api_key(my_key, install = TRUE)
+
 #Data is on external drive
 #check for files
 list.files("/Volumes/LaCie/DropBox/Github/data/")
-# Corrected file path (remove trailing quote)
+# Corrected file path 
 file_path <- "/Volumes/LaCie/DropBox/Github/data/AHRF 2023-2024 CSV/ahrf2024_Feb2025.csv"  
 
 # Read CSV
@@ -11,7 +29,7 @@ ahrf_data <- read.csv(file_path, stringsAsFactors = FALSE)
 head(ahrf_data)
 str(ahrf_data)
 
-# Corrected file path (remove trailing quote)
+# Corrected file path 
 file_path <- "/Volumes/LaCie/DropBox/Github/data/PLACES__Local_Data_for_Better_Health,_County_Data_2024_release_20251128.csv"  
 
 # Read CSV
